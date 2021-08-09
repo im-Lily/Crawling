b8f936193983fc477c4ccf65b5dc56b96ca40273
im-Lily <dmsruddlvhs2013@gmail.com>
2021-08-09 18:11:02 +0900

    update insertDB.py

--- book/insertDB.py
+++ book/insertDB.py
@@ -1,24 +1,39 @@
+import sys
+import os
 import pymysql
+import logging
+import time
 
 host_name = "shop-book.ciclmc7wjq3o.ap-northeast-2.rds.amazonaws.com"
+port = 3306
 user_name = "shopmaster"
 password = "springbook11"
 database_name = "shop_book"
 
-db = pymysql.connect(
-    host=host_name,
-    port=3306,
-    user=user_name,
-    passwd=password,
-    db=database_name,
-    charset="utf8"
-)
+def connect_RDS(host_name,port,user_name,password,database_name) :
+    try :
+        conn = pymysql.connect(
+        host=host_name,
+        port=port,
+        user=user_name, 
+        passwd=password,
+        db=database_name,
+        charset="utf8")
+        cursor = conn.cursor()
+    except :
+        logging.error("RDS 연결 실패")
+        sys.exit(1)
 
-cursor = db.cursor()
+    return conn, cursor
 
-sql = "INSERT INTO Book (book_id, book_nm, writer, thumbnail_url, book_info_url, price) VALUES (%s, %s, %s, %s, %s, %s)"
-val = (1,'a','b','c','d',2)
-cursor.execute(sql, val)
-db.commit()
-db.close()
+def main() :
+    conn, cursor = connect_RDS(host_name,port,user_name,password,database_name)
+
+    query = "INSERT INTO Book (book_id, book_nm, writer, thumbnail_url, book_info_url, price) VALUES (2,'a','b','c','d',3)"
+    cursor.execute(query)
+    conn.commit()
+
+if __name__ == "__main__" :
+    main()
+
 
