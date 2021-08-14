a52b7aa82e07fa94244ab5016d0f5f9aa0099011
im-Lily <dmsruddlvhs2013@gmail.com>
2021-08-15 03:04:30 +0900

    국어와 외국어 카테고리 DB 적재

--- book/crawling.py
+++ book/crawling.py
@@ -13,26 +13,23 @@ from bs4 import BeautifulSoup
 
 url = "https://book.naver.com/bestsell/bestseller_list.nhn"
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.implicitly_wait(3)
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver.implicitly_wait(3)
+
+driver = webdriver.Chrome('C:/Users/dmsru/Desktop/shopping/book/chromedriver.exe')
+time.sleep(3)
 
 driver.get(url)
 bsObject = BeautifulSoup(driver.page_source, 'html.parser')
 
 # 소설 카테고리부터 시작, 1페이지
-driver.find_element_by_xpath('//*[@id="memo1"]/div[1]/ul/li[2]').click()
+# driver.find_element_by_xpath('//*[@id="memo1"]/div[1]/ul/li[2]').click()
+# time.sleep(3)
+
+# 국어와 외국어
+driver.find_element_by_xpath('//*[@id="memo1"]/div[1]/ul/li[10]').click()
 time.sleep(3)
 
-# 카테고리 이동(2,29)
-# def cateBtn() :
-# for i in range(3,6) :
-#     cate = driver.find_element_by_xpath(f'//*[@id="memo1"]/div[1]/ul/li[{i}]').click()
-#     time.sleep(2)
-    # return cate
-
-# def pageBtn() :
-# 페이지 이동(1,6)
-# 첫 페이지 디폴트
 
 book_page_urls = []
 for i in range(1,7) :
@@ -45,7 +42,7 @@ for i in range(1,7) :
     time.sleep(3)   
     
 print('총 개수--->', len(book_page_urls))
-print('url---->',book_page_urls)
+# print('url---->',book_page_urls)
 
 
 # 상세 페이지(0,25)
@@ -59,47 +56,56 @@ print('url---->',book_page_urls)
 
 # 메타 정보와 본문에서 필요한 정보를 추출합니다.
 
-def book_data():
-    all_book = []
-    for index, book_page_url in enumerate(book_page_urls):
+# def book_data():
+all_book = []
+for index, book_page_url in enumerate(book_page_urls):
 
-        driver.get(book_page_url)
-        bsObject = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.get(book_page_url)
+    bsObject = BeautifulSoup(driver.page_source, 'html.parser')
 
-        title = bsObject.find('meta', {'property':'og:title'}).get('content')
-        author = bsObject.find('dt', text='저자').find_next_siblings('dd')[0].text.strip()
-        publisher = bsObject.find('dt', text='출판사').find_next_sibling('dd').text
-        dd = bsObject.find('dt', text='가격').find_next_siblings('dd')[0]
-        price = dd.select('div.lowest span.price')[0].text
-        # isbn = driver.find_element_by_xpath('//*[@id="container"]/div[4]/div[1]/div[2]/div[3]/text()[2]')
-        # print('isbn--->',isbn)
-        #     # isbn_tag = '#isbnBtn'
-        #     # isbn = bsObject.select(isbn_tag)
-        #     # print("isbn 제발!!", isbn)
+    title = bsObject.find('meta', {'property':'og:title'}).get('content')
+    author = bsObject.find('dt', text='저자').find_next_siblings('dd')[0].text.strip()
+    publisher = bsObject.find('dt', text='출판사').find_next_sibling('dd').text
+    # dd = bsObject.find('dt', text='가격').find_next_siblings('dd')[0]
+    try :
+        # price = dd.select('div.lowest span.price')[0].text
+        price = bsObject.select_one('div.lowest > span.price').text
+        print('2개 가격의 개수---->', len(price))
+        print('2개 가격---->',price)
+    except :
+        price = bsObject.select_one('div.retail > strong').text
+        print('1개 가격---->',price)
+        print('1개 가격의 개수---->', len(price))
+    # isbn = driver.find_element_by_xpath('//*[@id="container"]/div[4]/div[1]/div[2]/div[3]/text()[2]')
+    # print('isbn--->',isbn)
+    #     # isbn_tag = '#isbnBtn'
+    #     # isbn = bsObject.select(isbn_tag)
+    #     # print("isbn 제발!!", isbn)
 
-        image = bsObject.find('meta', {'property':'og:image'}).get('content')
-        if bsObject.find('h3', text='목차') == None :
-            print('title--->',title)
-            continue
-        section = bsObject.find('h3', text='목차').find_next_sibling('div').text
-        # section = section.replace('\n','')
-        description = bsObject.find('meta', {'property':'og:description'}).get('content')
-        # description = description.replace('\n','')
+    image = bsObject.find('meta', {'property':'og:image'}).get('content')
+    if bsObject.find('h3', text='목차') == None :
+        # print('title--->',title)
+        continue
+    section = bsObject.find('h3', text='목차').find_next_sibling('div').text
+    # section = section.replace('\n','')
+    description = bsObject.find('meta', {'property':'og:description'}).get('content')
+    # description = description.replace('\n','')
 
-        title_info = [];author_info = [];publisher_info = [];price_info = [];image_info = [];section_info = [];description_info = [];
+    title_info = [];author_info = [];publisher_info = [];price_info = [];image_info = [];section_info = [];description_info = [];
 
-        title_info.append(title)
-        author_info.append(author)
-        publisher_info.append(publisher)
-        price_info.append(price)
-        image_info.append(image)
-        section_info.append(section)
-        description_info.append(description)
+    title_info.append(title)
+    author_info.append(author)
+    publisher_info.append(publisher)
+    price_info.append(price)
+    image_info.append(image)
+    section_info.append(section)
+    description_info.append(description)
 
-        book_info = [book for book in zip(title_info,author_info,publisher_info,price_info,image_info,section_info,description_info)]
-        all_book.append(book_info[0])
-                
-    return all_book
+    book_info = [book for book in zip(title_info,author_info,publisher_info,price_info,image_info,section_info,description_info)]
+    all_book.append(book_info[0])
+    # print('all--->, all_book')
+                    
+    # return all_book
 
 # print('all--->',all_book)    
 
