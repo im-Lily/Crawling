4ff47835cfba5578cb1d1a0dfdc466c63b30b603
im-Lily <dmsruddlvhs2013@gmail.com>
2023-02-14 23:26:09 +0900

    feat: add page example

--- lecture/Chapter03/linkEx.py
+++ lecture/Chapter03/linkEx.py
@@ -1,11 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EC%98%AC%EB%A6%AC%EB%B8%8C%EC%98%81")
-html = response.text
-soup = BeautifulSoup(html, 'html.parser')
-links = soup.select(".news_tit") # 결과는 리스트
-for link in links:
-    title = link.text # 태그 안의 텍스트 요소를 가져옴
-    url = link.attrs['href'] # href 속성값을 가져옴
-    print(title, url)
+keyword = input("검색어를 입력하세요: ")
+lastPage = int(input("마지막 페이지 번호를 입력해주세요: "))
+pageNum = 1
+for i in range(1, lastPage*10, 10):
+    print(f"{pageNum}페이지입니다. ===================================")
+    response = requests.get(f"https://search.naver.com/search.naver?where=news&sm=tab_jum&query={keyword}&start={i}")
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.select(".news_tit")  # 결과는 리스트
+    for link in links:
+        title = link.text  # 태그 안의 텍스트 요소를 가져옴
+        url = link.attrs['href']  # href 속성값을 가져옴
+        print(title, url)
+    pageNum = pageNum + 1
