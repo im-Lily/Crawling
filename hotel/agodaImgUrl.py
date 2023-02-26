26d3d6d808581a5a6c489fe5369023b5edec59e1
im-Lily <dmsruddlvhs2013@gmail.com>
2023-02-27 00:41:47 +0900

    finished

--- hotel/agodaImgUrl.py
+++ hotel/agodaImgUrl.py
@@ -29,12 +29,13 @@ driver.get(
 
 # 로딩이 끝날 때까지 5초 기다리기
 driver.implicitly_wait(5)
+SCROLL_PAUSE_TIME = 20
 
 img_url_info = {}
 
 
 # 이미지 url 가져오는 함수
-def get_img_url(driver):
+def get_img_url(driver, next_button):
     # 숙소 외관 이미지
     mainImgList = driver.find_elements(By.CLASS_NAME, "HeroImage.HeroImage--s")
     mainImgId = 0
@@ -59,8 +60,14 @@ def get_img_url(driver):
 
         time.sleep(10)
 
+        # 다음 페이지로 이동
+        if next_button.is_displayed():
+            next_button.click()
+            time.sleep(SCROLL_PAUSE_TIME)
+
     except Exception as e:
-        print("e: ", e)
+        print("get_img_url exception: ", e)
+        driver.find_element(By.CLASS_NAME, "Box-sc-kv6pi1-0.dmiRkO").click()
     pass
 
 
@@ -70,72 +77,46 @@ def get_detail_img_url(driver):
     tabList = driver.find_elements(By.CLASS_NAME,
                                    "Buttonstyled__ButtonStyled-sc-5gjk6l-0.izeZQM")
 
-    # 객실 수 저장
-    roomLen = int(re.sub(r'[^0-9]', '', tabList[2].text))
+    try:
+        # 객실 수 저장
+        roomLen = int(re.sub(r'[^0-9]', '', tabList[2].text))
 
-    # 객실 클릭
-    tabList[2].click()
-    time.sleep(2)
+        # 객실 클릭
+        tabList[2].click()
+        time.sleep(2)
 
-    # 상세(객실) 이미지 - 원본 이미지
-    elements = driver.find_elements(By.CLASS_NAME,
-                                    "Box-sc-kv6pi1-0 > img")
-    count = 4
-    while True:
-        detailImgUrl = elements[count].get_attribute("src")
-        img_url_info['detailImgUrl'] = detailImgUrl
+        print(">", tabList[2].text)
+        print(">>", "객실" + "(" + str(roomLen) + ")")
 
-        # 상세 이미지 개수 5개 가져오기 or 상세 이미지 개수가 5개보다 적은 경우 종료
-        if (count == 9 or count > roomLen):
-            # 상세 이미지 창 닫기
+        # 객실 이미지만 가져오기
+        if(tabList[2].text == "객실" + "(" + str(roomLen) + ")"):
+
+            # 상세(객실) 이미지 - 원본 이미지
+            elements = driver.find_elements(By.CLASS_NAME,
+                                            "Box-sc-kv6pi1-0 > img")
+            count = 4
+            while True:
+                detailImgUrl = elements[count].get_attribute("src")
+                img_url_info['detailImgUrl'] = detailImgUrl
+
+                # 상세 이미지 개수 5개 가져오기 or 상세 이미지 개수가 5개보다 적은 경우 종료
+                if (count == 9 or count > roomLen):
+                    # 상세 이미지 창 닫기
+                    driver.find_element(By.CLASS_NAME, "Box-sc-kv6pi1-0.dmiRkO").click()
+                    break
+
+                # 상세 이미지 url DB 저장
+                save_detail_data(img_url_info)
+                count += 1
+        else:
             driver.find_element(By.CLASS_NAME, "Box-sc-kv6pi1-0.dmiRkO").click()
-            break
-
-        # 상세 이미지 url DB 저장
-        save_detail_data(img_url_info)
-        count += 1
-
-
-# 스크롤 내리면서 페이지 이동하는 함수
-# def scroll_to_bottom(driver):
-#     SCROLL_PAUSE_TIME = 15
-#     last_height = driver.execute_script("return document.body.scrollHeight")
-#     while True:
-#         # 끝까지 스크롤
-#         driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-#         time.sleep(SCROLL_PAUSE_TIME)
-#         # 스크롤 다운 후 스크롤 높이 다시 가져옴
-#         new_height = driver.execute_script("return document.body.scrollHeight")
-#
-#         # 스크롤 끝까지 내렸을 때
-#         if new_height == last_height:
-#             # 맨 위로 올라감
-#             driver.execute_script("window.scrollTo(0, 0)")
-#             time.sleep(SCROLL_PAUSE_TIME)
-#
-#             # 이미지 url 가져오는 함수 호출
-#             get_img_url(driver)
-#
-#             try:
-#                 # 다음 페이지로 이동
-#                 next_button = driver.find_element(By.CSS_SELECTOR, "#paginationNext")
-#                 if next_button.is_displayed():
-#                     next_button.click()
-#                     time.sleep(SCROLL_PAUSE_TIME)
-#                 else:
-#                     break  # 마지막 페이지인 경우 while loop 종료
-#             except:
-#                 break  # 다음 페이지 버튼이 없는 경우 while loop 종료
-#
-#             # 스크롤 위치 초기화
-#             last_height = 0
-#
-#         else:
-#             last_height = new_height
+    except Exception as e:
+        print("get_detail_img_url exception: ", e)
+        driver.find_element(By.CLASS_NAME, "Box-sc-kv6pi1-0.dmiRkO").click()
+        pass
 
 def scroll_to_bottom(driver):
     # 스크롤 끝까지 내리기
-    SCROLL_PAUSE_TIME = 20
 
     before_location = driver.execute_script("return window.pageYOffset")
 
@@ -146,20 +127,22 @@ def scroll_to_bottom(driver):
         # 스크롤 다운 후 스크롤 높이 다시 가져옴
         after_location = driver.execute_script("return window.pageYOffset")
 
-        get_img_url(driver)
-
         # 마지막 스크롤일 때
         if before_location == after_location:
-            try:
-                # 다음 페이지로 이동
-                next_button = driver.find_element(By.CSS_SELECTOR, "#paginationNext")
-                if next_button.is_displayed():
-                    next_button.click()
-                    time.sleep(SCROLL_PAUSE_TIME)
-                else:
-                    break  # 마지막 페이지인 경우 while loop 종료
-            except:
-                break  # 다음 페이지 버튼이 없는 경우 while loop 종료
+
+            next_button = driver.find_element(By.CSS_SELECTOR, "#paginationNext")
+            get_img_url(driver, next_button)
+
+        # try:
+        #     # 다음 페이지로 이동
+
+        #     if next_button.is_displayed():
+        #         next_button.click()
+        #         time.sleep(SCROLL_PAUSE_TIME)
+        #     else:
+        #         break  # 마지막 페이지인 경우 while loop 종료
+        # except:
+        #     break  # 다음 페이지 버튼이 없는 경우 while loop 종료
         else:
             # 이동여부 판단 기준이 되는 이전 위치 값 수정
             before_location = driver.execute_script("return window.pageYOffset")
